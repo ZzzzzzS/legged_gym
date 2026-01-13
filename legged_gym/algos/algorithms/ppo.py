86a371ad6af31de21175198ea41d09938190f853
ZzzzzzS <zhouzishun@mail.zzshub.cn>
2026-01-13 10:32:12 +0800

    add entropy_loss log

--- legged_gym/algos/algorithms/ppo.py
+++ legged_gym/algos/algorithms/ppo.py
@@ -138,6 +138,7 @@ class PPO:
     def update(self):
         mean_value_loss = 0
         mean_surrogate_loss = 0
+        entropy_loss = 0
         if self.actor_critic.is_recurrent:
             generator = self.storage.reccurent_mini_batch_generator(self.num_mini_batches, self.num_learning_epochs)
         else:
@@ -213,10 +214,12 @@ class PPO:
 
             mean_value_loss += value_loss.item()
             mean_surrogate_loss += surrogate_loss.item()
+            entropy_loss += entropy_batch.mean().item()
 
         num_updates = self.num_learning_epochs * self.num_mini_batches
         mean_value_loss /= num_updates
         mean_surrogate_loss /= num_updates
+        entropy_loss /= num_updates
         self.storage.clear()
 
-        return {"mean_value_loss": mean_value_loss, "mean_surrogate_loss": mean_surrogate_loss}
+        return {"mean_value_loss": mean_value_loss, "mean_surrogate_loss": mean_surrogate_loss, "entropy_loss": entropy_loss}
